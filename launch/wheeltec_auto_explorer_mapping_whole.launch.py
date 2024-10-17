3fceb6e389fa7d4086590f4955e32647e344bf8f
wei.sun <sunweijm.sun@gmail.com>
2024-10-17 16:57:41 +0800

    add save map function by navigation2 nav2_map_server

--- launch/wheeltec_auto_explorer_mapping_whole.launch.py
+++ launch/wheeltec_auto_explorer_mapping_whole.launch.py
@@ -12,6 +12,8 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+import launch_ros.actions
+
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
@@ -21,6 +23,7 @@ def generate_launch_description():
     nav2_launch_file_dir = os.path.join(get_package_share_directory('open_source_slam_launch'), 'launch')
     robot_pose_publisher_launch_file_dir = os.path.join(get_package_share_directory('robot_pose_publisher'), 'launch')
     auto_explore_mapper_launch_file_dir = os.path.join(get_package_share_directory('auto_explore_mapper'), 'launch')
+    nav2_map_server_launch_file_dir = os.path.join(get_package_share_directory('nav2_map_server'), 'launch')
 
     return LaunchDescription([
 
@@ -51,6 +54,14 @@ def generate_launch_description():
 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([auto_explore_mapper_launch_file_dir, '/auto_explore_mapper_d2_cartographer.launch.py']),
+            launch_arguments={
+                'use_sim_time': use_sim_time,
+                }.items(),
+        ),
+
+        #launch navigation2 nav2_map_server
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([nav2_map_server_launch_file_dir, '/map_saver_server.launch.py']),
             launch_arguments={
                 'use_sim_time': use_sim_time,
                 }.items(),
